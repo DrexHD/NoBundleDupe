15c5e65f00795bad320f74f25181cc16f540427a
DrexHD <nicknamedrex@gmail.com>
2020-11-06 15:05:23 +0100

    use proper packages

--- src/main/java/me/drex/nobundledupe/mixin/BundleItemMixin.java
+++ src/main/java/me/drex/nobundledupe/mixin/BundleItemMixin.java
@@ -8,10 +8,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BundleItem.class)
-public class BundleItemMixin {
+public abstract class BundleItemMixin {
 
     @Redirect(method = "method_31562", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;method_31574(Lnet/minecraft/item/Item;)Z"))
     private static boolean denySpecialCaseBundle(ItemStack itemStack, Item item) {
         return false;
     }
+
 }
