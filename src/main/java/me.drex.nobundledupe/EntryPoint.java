cfde1d5505eb20836842a126b1b9dbeebd32f8f4
DrexHD <nicknamedrex@gmail.com>
2020-11-06 11:36:25 +0100

    update examples

---
+++ src/main/java/me.drex.nobundledupe/EntryPoint.java
@@ -0,0 +1,11 @@
+package me.drex.nobundledupe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EntryPoint implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
